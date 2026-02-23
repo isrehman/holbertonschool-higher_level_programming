06c1d1dfa21ebe4fe1562a9ed69c7a3adeb8bd99
isrehman <isrehman2006@gmail.com>
2026-02-23 19:22:41 +0000

    Add 8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Python is an interpreted language"
+str2 = "Programming is like building a multilingual puzzle"
+str3 = "object-oriented programming with Python"
+print(str3)
