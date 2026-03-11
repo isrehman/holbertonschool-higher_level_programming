aafe19431cedd0d6dccfde106707aea7923e9cd9
isrehman <isrehman2006@gmail.com>
2026-03-11 12:33:52 +0000

    Fix stats to print at end of input

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
             line_count += 1
             if line_count % 10 == 0:
                 print_stats(total_size, status_codes)
+        print_stats(total_size, status_codes)
     except KeyboardInterrupt:
         print_stats(total_size, status_codes)
         raise
