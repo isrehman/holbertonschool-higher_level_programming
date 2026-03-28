c6a86216809ca466e826da2452db74f51505d4b0
isrehman <isrehman2006@gmail.com>
2026-03-28 12:01:31 +0400

    Add task 3: error code handling

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that sends a request and handles HTTP error codes"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
