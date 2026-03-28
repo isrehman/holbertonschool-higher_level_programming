340899f50737f70467eaa3308ca20e4098e3ffae
isrehman <isrehman2006@gmail.com>
2026-03-28 11:58:18 +0400

    Add python-network_1 README and task 0: fetch status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status using urllib"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = urllib.request.Request(url, headers={'cfclearance': 'true'})
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
